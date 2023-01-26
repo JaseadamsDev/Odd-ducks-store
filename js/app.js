a09f837f855cfe0e9e6c894d6cfa9291a25f5de6
Jason Adams <jayadams23@gmail.com>
2023-01-26 14:02:19 -0800

    app.js first commit

--- js/app.js
+++ js/app.js
@@ -14,15 +14,19 @@ const Cart = function(items) {
 
 Cart.prototype.addItem = function(product, quantity) {
   // TODO: Fill in this instance method to create a new CartItem and add it to this.items
+  let newCartItem = new CartItem(product, quantity);
+  this.items.push(newCartItem);
 };
 
 Cart.prototype.saveToLocalStorage = function() {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
+localStorage.setItem('cart', JSON.stringify(this.items));
 };
 
 Cart.prototype.removeItem = function(item) {
   // TODO: Fill in this instance method to remove one item from the cart.
   // Note: You will have to decide what kind of parameter to pass in here!
+this.items.splice(item, 1);
 };
 
 Cart.prototype.updateCounter = function() {
@@ -38,7 +42,9 @@ const CartItem = function(product, quantity) {
 const Product = function(filePath, name) {
   this.filePath = filePath;
   this.name = name;
+  product.allProducts.push(this);
 };
+product.allProducts = [];
 
 function generateCatalog() {
   let bag = new Product('assets/bag.jpg', 'Bag');
