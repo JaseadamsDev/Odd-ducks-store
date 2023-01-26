0eedda69a6b439fcb6c262f469c663e4be78c914
jamesCodes808 <jamesian.r.solima@gmail.com>
2023-01-26 15:00:13 -0800

    james-test2

--- js/catalog.js
+++ js/catalog.js
@@ -95,3 +95,6 @@ catalogForm.addEventListener('submit', handleSubmit);
 // Before anything else of value can happen, we need to fill in the select
 // drop down list in the form.
 populateForm();
+
+
+//test
